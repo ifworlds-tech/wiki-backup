b46bbd4bbf12913816c7825de3197640283910ea
AtsunaAkiya <atsunaakiya@outlook.com>
2021-03-08 09:34:22 +0800

    bugfix

--- backup.py
+++ backup.py
@@ -6,8 +6,8 @@ site = mwclient.Site("wiki.ifworlds.org")
 root = Path("data")
 
 ns_list = [
-    ('主空间', ''),
     ('模板', '模板'),
+    ('主空间', ''),
     ('模块', '模块'),
     ('概念', '概念'),
     ('属性', '属性'),
@@ -30,9 +30,9 @@ if __name__ == "__main__":
     for ns_name, ns in ns_list:
         ns_id = reversed_ns[ns]
         for p in site.allpages(namespace=ns_id):
-            path = root / ns_name / f"{p.name}.txt"
+            path = root / ns_name / f"{p.page_title}.txt"
             path.parent.mkdir(exist_ok=True, parents=True)
-            print(f"{ns}:{p.name}")
+            print(f"{ns}:{p.page_title}")
             with open(path, 'w') as f:
                 f.write(p.text())
             time.sleep(2)
