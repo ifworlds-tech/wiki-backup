e3c4f3b54905693c7dc933e993aaf9cf26fdfa82
AtsunaAkiya <atsunaakiya@outlook.com>
2021-03-08 08:35:26 +0800

    init

--- backup.py
+++ backup.py
@@ -1,9 +1,15 @@
+import time
 import mwclient
 from pathlib import Path
 
 site = mwclient.Site("wiki.ifworlds.org")
+root = Path("data")
 
 if __name__ == "__main__":
     for p in site.pages:
-        print(p.text())
-        break
+        path = root / f"{p.name}.txt"
+        path.parent.mkdir(exist_ok=True, parents=True)
+        print(p.name)
+        with open(path, 'w') as f:
+            f.write(p.text())
+        time.sleep(2)
